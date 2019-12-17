9881b63e34fa12b69c91398a552a1f3b0bb0785e
EfiOpenSourcePRJ <leadproject2015@gmail.com>
2019-12-17 10:29:39 +0200

    add changes

---
+++ CrudOp/src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { ProductEditComponent } from './components/product-edit/product-edit.component';
+import { ProductGetComponent } from './components/product-get/product-get.component';
+
+const routes: Routes = [
+  {
+    path: "product/create",
+    component: ProductAddComponent
+  },
+  {
+    path: "edit/:id",
+    component: ProductEditComponent
+  },
+  {
+    path: "products",
+    component: ProductGetComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
